13903444ee32f2d867aad2dcbe6c110b3f2e61b6
NidaWaheed <109359002+Nidachaudary@users.noreply.github.com>
2024-12-01 01:47:10 +0500

    login  page build + redux toolkit do

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
+
 import Body from "./components/Body";
+import Feed from "./Pages/Feed";
 import Login from "./Pages/Login";
 import Profile from "./Pages/Profile";
+
 function App() {
   return (
     <>
-      <BrowserRouter basename="/">
-        <Routes>
-          <Route path="/" element={<Body />}>
-            <Route path="/login" element={<Login />} />
-            <Route path="/profile" element={<Profile />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <Provider store={appStore}>
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route path="/" element={<Body />}>
+              <Route path="/" element={<Feed />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/profile" element={<Profile />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </>
   );
 }
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,7 +1,73 @@
-import React from "react";
-
+import { useState } from "react";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constants";
 const Login = () => {
-  return <div>Login</div>;
+  const [emailId, setEmailId] = useState("sonia@gmail.com");
+  const [password, setPassword] = useState("sonia@123N");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  //login handler
+  const loginHandler = async () => {
+    try {
+      const res = await axios.post(
+        BASE_URL + "/signIn",
+        {
+          emailId,
+          password,
+        },
+        { withCredentials: true }
+      );
+
+      dispatch(addUser(res.data));
+      return navigate("/");
+    } catch (err) {
+      console.log(err.response.data);
+    }
+  };
+  return (
+    <div className="flex justify-center mt-12">
+      <div className="card bg-base-300 w-96 shadow-xl ">
+        <div className="card-body">
+          <h2 className="card-title flex justify-center">Login</h2>
+
+          <label className="form-control w-full max-w-xs">
+            <div className="label">
+              <span className="label-text">Email ID</span>
+            </div>
+            <input
+              type="text"
+              value={emailId}
+              onChange={(e) => setEmailId(e.target.value)}
+              placeholder="Type here"
+              className="input input-bordered w-full max-w-xs"
+            />
+          </label>
+
+          <label className="form-control w-full max-w-xs">
+            <div className="label">
+              <span className="label-text">Password</span>
+            </div>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Type here"
+              className="input input-bordered w-full max-w-xs"
+            />
+          </label>
+
+          <div className="card-actions flex justify-center mt-4">
+            <button onClick={loginHandler} className="btn btn-primary">
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
